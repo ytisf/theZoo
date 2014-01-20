e2ae3ccb25468c6b00b803c943c55b17b83892bc
Yuval Nativ <yuvaln210@gmail.com>
2014-01-20 10:21:05 +0200

    Beta of Beta

--- Rebuild_CSV.sh
+++ Rebuild_CSV.sh
@@ -8,22 +8,22 @@ red_min='\e[01;31m[-]\e[00m'
 # This file rebuilds the index.csv file based on the local index.log file in each folder.
 
 # Backup previous 
-mv index.csv Index.Backup.csv
+mv conf/index.csv conf/Index.Backup.csv
 
 # finds all index.log files:
 
 find `pwd` -name 'index.log' > /tmp/indexrebuild.tmp
-touch index.csv
+touch conf/index.csv
 i=1
 cat /tmp/indexrebuild.tmp | while read file ; do
 	let string="$i"
 	string="$string,`echo "$file"`,`cat "$file"`,"
 	echo -e "$green_plus $i was added successfully"
-	echo "$string" >> index.csv
+	echo "$string" >> conf/index.csv
 	let i=i+1
 done
 
-linesofdb=`wc -l < index.csv`
+linesofdb=`wc -l < conf/index.csv`
 
 if [ $linesofdb = 0 ]; then
 	echo ""
