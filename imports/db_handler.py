a7f3adc5f5e60ab2487f20b104f1400e01116841
tisf <ytisf@users.noreply.github.com>
2025-06-21 17:04:57 +0700

    dbVer --> 1750498935000
    Some fresh wipers

--- imports/db_handler.py
+++ imports/db_handler.py
@@ -1,3 +1,4 @@
+python
 import sqlite3 as lite
 from imports import globals
 import sys
@@ -6,59 +7,61 @@ import sys
 class DBHandler:
 
     def __init__(self):
+        self._connect()
+
+    def _connect(self):
         try:
             self.con = lite.connect(globals.vars.db_path)
             self.cur = self.con.cursor()
         except lite.Error as e:
-            print("An error occurred:", e.args[0])
+            print(f"An error occurred: {e.args[0]}")
             sys.exit()
 
     def get_full_details(self):
         return self.cur.execute("SELECT * FROM Malwares").fetchall()
 
     def get_partial_details(self):
-        return self.cur.execute("SELECT ID, TYPE, LANGUAGE, ARCHITECTURE, PLATFORM, NAME FROM Malwares").fetchall()
+        query = "SELECT ID, TYPE, LANGUAGE, ARCHITECTURE, PLATFORM, NAME FROM Malwares"
+        return self.cur.execute(query).fetchall()
 
     def get_mal_list(self):
-        return self.cur.execute("SELECT ID, NAME, TYPE From Malwares").fetchall()
+        query = "SELECT ID, NAME, TYPE FROM Malwares"
+        return self.cur.execute(query).fetchall()
 
     def get_mal_names(self):
-
-        # Sqlite3 returns a tuple even if a single value is returned
-        # We use x[0] for x to unpack the tuples
-        return [val[0] for val in self.cur.execute("SELECT NAME FROM Malwares").fetchall()]
+        query = "SELECT NAME FROM Malwares"
+        return [val[0] for val in self.cur.execute(query).fetchall()]
 
     def get_mal_tags(self):
-        return [val[0] for val in self.cur.execute("SELECT DISTINCT TAGS From Malwares WHERE TAGS IS NOT NULL").fetchall()]
+        query = "SELECT DISTINCT TAGS FROM Malwares WHERE TAGS IS NOT NULL"
+        return [val[0] for val in self.cur.execute(query).fetchall()]
 
     def get_mal_info(self, mid):
-        return self.cur.execute("SELECT TYPE, NAME, VERSION, AUTHOR, LANGUAGE, DATE, ARCHITECTURE, PLATFORM, TAGS From Malwares WHERE ID =" + str(mid)).fetchall()
+        query = (
+            "SELECT TYPE, NAME, VERSION, AUTHOR, LANGUAGE, DATE, ARCHITECTURE, PLATFORM, TAGS "
+            f"FROM Malwares WHERE ID = {mid}"
+        )
+        return self.cur.execute(query).fetchall()
 
     def query(self, query, param=''):
-        if globals.vars.DEBUG_LEVEL is 2:
+        if globals.vars.DEBUG_LEVEL == 2:
             print(locals())
         try:
-            if param is not '':
-                return self.cur.execute(query, param if type(param) is list else [param]).fetchall()
-            else:
-                return self.cur.execute(query).fetchall()
+            if param != '':
+                params = param if isinstance(param, list) else [param]
+                return self.cur.execute(query, params).fetchall()
+            return self.cur.execute(query).fetchall()
         except lite.Error as e:
-            print("An error occurred:", e.args[0])
+            print(f"An error occurred: {e.args[0]}")
             sys.exit()
 
     def close_connection(self):
         try:
             self.cur.close()
             self.con.close()
-            return
         except lite.Error as e:
-            print("An error occurred:", e.args[0])
+            print(f"An error occurred: {e.args[0]}")
             sys.exit()
 
     def renew_connection(self):
-        try:
-            self.con = lite.connect(globals.vars.db_path)
-            self.cur = self.con.cursor()
-        except lite.Error as e:
-            print("An error occurred:", e.args[0])
-            sys.exit()
+        self._connect()
